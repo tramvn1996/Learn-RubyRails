9cf377b509c4a85b14873f1a3868536f448de702
tramvn1996 <tramvn1996@gmail.com>
2018-08-23 15:45:42 -0400

    add validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,12 +11,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
-  @article = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
 
-  @article.save
-  redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
-
   private
   def article_params
     params.require(:article).permit(:title, :text)
